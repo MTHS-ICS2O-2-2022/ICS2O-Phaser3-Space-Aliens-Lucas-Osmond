3914d132fa2366819d2ec1b9d86b7b52bf3073d7
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-04-14 17:01:20 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
+/* global Phaser */
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Osmond
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+/**
+ *Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    }, 
+  },
+  //set background colour
+  backgroundColor: '0x5f6e7a',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //we place the game in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
