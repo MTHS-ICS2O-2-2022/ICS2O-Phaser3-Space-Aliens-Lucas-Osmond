18aeb59c447fc81baa9b2d80d46bf63d879a29bb
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-04-20 16:53:45 +0000

    update

--- js/game.js
+++ js/game.js
@@ -18,6 +18,12 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    },
+  },
   //set background colour
   backgroundColor: '0x5f6e7a',
   scale: {
