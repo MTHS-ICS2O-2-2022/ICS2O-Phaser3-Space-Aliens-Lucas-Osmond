4d7638a5f96c697f60f778a583bbd5c7933121fb
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-18 17:01:05 +0000

    update

--- js/gameScene.js
+++ js/gameScene.js
@@ -12,6 +12,7 @@
 class GameScene extends Phaser.Scene {
   // create an alien
   createAlien () {
+    console.log('test alien')
     const alien = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
     let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 200
     alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
@@ -74,7 +75,7 @@ class GameScene extends Phaser.Scene {
     this.missileGroup = this.physics.add.group()
 
     // create a group for the aliens
-    this.alienGroup = this.physics.add.group()
+    this.alienGroup = this.add.group()
     this.createAlien()
   }
 
