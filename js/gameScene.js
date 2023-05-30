ad3944dbb9105517e0d201b2e78ce683e6d4f266
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-30 00:24:42 +0000

    update

--- js/gameScene.js
+++ js/gameScene.js
@@ -37,6 +37,11 @@ class GameScene extends Phaser.Scene {
       fill: "#ffffff",
       align: "center",
     }
+    this.gameOverTextStyle = {
+      font: "65px Arial",
+      fill: "#ff0000",
+      align: "center",
+    }
   }
 
   /**
@@ -64,6 +69,7 @@ class GameScene extends Phaser.Scene {
     // load sounds
     this.load.audio("laser", "assets/laser1.wav")
     this.load.audio("explosion", "assets/barrelExploding.wav")
+    this.load.audio('bomb', 'assets/bomb.wav')
   }
 
   /**
@@ -100,7 +106,19 @@ class GameScene extends Phaser.Scene {
         this.createAlien()
       }.bind(this)
     )
-  }
+
+    // collision between ship and alien
+    this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+      this.sound.play("bomb")
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to Play Again.", this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCursor: true })
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+      }.bind(this)
+    )
+    }
 
   /**
    * Should be overridden on your own Scenes.
